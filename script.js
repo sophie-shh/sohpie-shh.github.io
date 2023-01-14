5ee88c88bc6c437a5ee604cd7a88116bbb0e6d1b
sophie-shh <122669090+sophie-shh@users.noreply.github.com>
2023-01-14 09:53:50 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
+
+getJSON('locale.json',
+function(err, data) {
+  if (err !== null) {
+    alert('Something went wrong: ' + err);
+  } else {
+    alert('Your query count: ' + data.query.count);
+  }
+});
